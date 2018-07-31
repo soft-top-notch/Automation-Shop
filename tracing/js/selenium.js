c6540904facce3cc4a1340bca259cdeb63cf89ea
andrew-f96 <ksso8901@gmail.com>
2018-07-31 17:46:17 +0300

    1. Select combobox values. 2. Few renamings + comments in selenium_helper

--- tracing/js/selenium.js
+++ tracing/js/selenium.js
@@ -24,40 +24,89 @@ window.__tra_simulateClick = function(elem) {
 }
 
 window.__tra_sleep = function(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+window.__tra_expandComboDropDown = async function(left, top, height, handler) {
+    var combo = document.elementFromPoint(left + 1, top + 1);
+
+    __tra_simulateClick(combo);
+    await __tra_sleep(500);
+    elem = document.elementFromPoint(left + 1, top + 1 + height);
+    while (true) {
+        var rect = elem.parentNode.getBoundingClientRect();
+        if (rect.x >=left && rect.y >= top) {
+            var elem = elem.parentNode;
+        }
+        else {
+        	break;
+        }
+    }
+    var found = elem.querySelectorAll('li');
+    var shouldClose = handler(found);
+
+    // close
+    if (shouldClose) {
+        __tra_simulateClick(combo);		
+   }
+   await __tra_sleep(500);
 }
 
 window.__tra_extractComboValues = async function(left ,top, height) {
-	x = left + 1;
-	y = top + 1;
-	combo = document.elementFromPoint(x, y);
-	if(combo.tagName === 'SELECT') {
-		found = combo.options;
-	}
-	else {
-		// open
-		__tra_simulateClick(combo);
-		await __tra_sleep(500);
-		elem = document.elementFromPoint(x, y + height);
-		while (true) {
-			var rect = elem.parentNode.getBoundingClientRect();
-			if (rect.x >=left && rect.y >= top) {
-				elem = elem.parentNode;
-			}
-			else {
-				break;
-			}
-		}
-		found = elem.querySelectorAll('li');
-        // close
-		__tra_simulateClick(combo);		
-		await __tra_sleep(500);
-	}
-	result = []
-	for (var i = 0; i < found.length; i++) {
-		result.push(found[i].textContent);
-	}
+    var x = left + 1,
+        y = top + 1;
+    var found = null;
+    var combo = document.elementFromPoint(x, y);
+    if (combo.tagName === 'SELECT') {
+        var found = combo.options;
+    }
+    else {
+        // expand
+       await __tra_expandComboDropDown(left, top, height, function(result){
+           found = result;
+           // Should close dropdown
+           return true;
+       })
+    }
+    var result = []
+    for (var i = 0; i < found.length; i++) {
+        result.push(found[i].textContent);
+    }
 
-	return result;
+    return result;
+}
+
+window.__tra_selectComboboxValue = async function(left, top, height, text_value) {
+    var x = left + 1,
+        y = top + 1;
+    var combo = document.elementFromPoint(x, y);
+    if (combo.tagName === 'SELECT') {
+        var found = combo.options;
+        console.log(found);	
+        for (var i = 0; i < found.length; ++i) {
+            if (found[i].textContent == text_value){
+                combo.value = found[i].value;
+                return true;
+            }
+        }
+        return false;
+    }
+    else {
+    	var result = false;
+    	await __tra_expandComboDropDown(left, top, height, function(found){
+            for (var i = 0; i < found.length; i++) {
+		        if (found[i].textContent == text_value) {
+		            __tra_simulateClick(found[i]);
+		            result = true;
+		            // Don't need to close dropdown
+		            return false;
+		        }
+		    }
+            // Found nothing, close dropdown explicitly
+            return true;
+        })
+
+        return result;
+    }
 }
 
